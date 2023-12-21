3d656a0fb9d46dd43867a39b0fbe71a77803f881
LEE <89049545+riot5@users.noreply.github.com>
2023-12-21 11:23:18 +0900

    Undead-Survivor
    Undead-Survivor

---
+++ Assets/Code/HUD.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using static Cinemachine.DocumentationSortingAttribute;
+
+public class HUD : MonoBehaviour
+{
+    // 5가지 정보
+    public enum InfoType { Exp , Level, Kill, Time, Health }
+    public InfoType type;
+
+    Text myText;
+    Slider mySlider;
+
+    void Awake()
+    {
+        myText = GetComponent<Text>();
+        mySlider = GetComponent<Slider>();
+    }
+
+    void LateUpdate()
+    {
+        switch (type)
+        {
+            case InfoType.Exp:
+                float curExp = GameManager.instance.exp;
+                float maxExt = GameManager.instance.nextExp[Mathf.Min(GameManager.instance.level, GameManager.instance.nextExp.Length - 1) ];
+                mySlider.value = curExp/maxExt;
+                break;
+
+            case InfoType.Level:
+                myText.text =string.Format("Lv.{0:F0}", GameManager.instance.level);
+                break;
+                
+            case InfoType.Kill:
+                myText.text = string.Format("{0:F0}", GameManager.instance.kill);
+                break;
+
+            case InfoType.Time:
+                float remaintime = GameManager.instance.maxGameTime - GameManager.instance.gameTime;
+                int min = Mathf.FloorToInt(remaintime / 60);
+                int sec = Mathf.FloorToInt(remaintime % 60);
+                myText.text  = string.Format("{0:D2}:{1:D2}",min, sec);
+
+                break;
+
+            case InfoType.Health:
+                float curHealth = GameManager.instance.health;
+                float maxHealth = GameManager.instance.maxHealth;
+                mySlider.value = curHealth / maxHealth;
+                break;
+          
+        }    
+    }
+
+}
---
+++ Assets/Code/Weapon.cs
@@ -0,0 +1,148 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+
+
+    public int id;
+    public int prefabId;
+    public float damage;
+    public int count;
+    public float speed;
+
+    float timer;
+    Player player;
+
+    void Awake()
+    {
+        player = GameManager.instance.player;    
+    }
+  
+    void Update()
+    {
+        if (!GameManager.instance.isLive)
+            return;
+
+        switch (id)
+        {
+            case 0:
+                transform.Rotate(Vector3.back * speed * Time.deltaTime);
+                break;        
+            default:
+                timer += Time.deltaTime;
+
+                if (timer > speed) 
+                {
+                    timer = 0f;
+                    Fire();
+                }
+                break;
+        }
+
+    }
+
+    public void LevelUp(float damage , int count)
+    {
+        this.damage = damage;
+        this.count = count;
+
+        if (id == 0)
+            Batch();
+
+        player.BroadcastMessage("ApplyGear", SendMessageOptions.DontRequireReceiver);
+
+    }
+
+    public void Init(ItemData data)
+    {
+
+        // basic set
+        name = "Weapon " + data.itemId;
+        transform.parent = player.transform;
+        transform.localPosition = Vector3.zero;
+        // property set
+        id = data.itemId;
+        damage = data.baseDamage * Character.Damage;
+        count = data.baseCount + Character.Count;
+
+        for(int index = 0; index < GameManager.instance.pool.prefabs.Length; index++) 
+        {
+            if(data.projectile == GameManager.instance.pool.prefabs[index] )
+            {
+                prefabId = index;
+                break;
+            }
+
+        }
+
+        // 무기 설정
+        switch (id)
+        {
+           
+            case 0:
+                speed = 150* Character.WeaponSpeed;
+                Batch();
+                break;
+            default:
+                speed = 0.3f*Character.WeaponRate;
+                break;
+        }
+
+        // Hand Set
+        Hand hand = player.hands[(int)data.itemType];
+        hand.spriter.sprite = data.hand;
+        hand.gameObject.SetActive(true);
+
+
+        player.BroadcastMessage("ApplyGear",SendMessageOptions.DontRequireReceiver);
+    }
+
+    void Batch()
+    {
+        for(int index=0; index < count; index++)
+        {
+            Transform bullet;
+
+            if (index < transform.childCount)
+            {
+                bullet = transform.GetChild(index);
+            }
+            else
+            {
+                bullet = GameManager.instance.pool.Get(prefabId).transform;
+                bullet.parent = transform;
+            }
+            
+            
+
+            bullet.localPosition = Vector3.zero;
+            bullet.localRotation = Quaternion.identity;
+
+            Vector3 rotVec = Vector3.forward * 360 * index / count;
+            bullet.Rotate(rotVec);
+            bullet.Translate(bullet.up * 1.5f, Space.World);
+
+            bullet.GetComponent<Bullet>().Init(damage, -100 , Vector3.zero); // -1 is Infinity per.
+
+
+        }
+    }
+    void Fire()
+    {
+        if (!player.scanner.nearestTarget)
+            return;
+
+        Vector3 targeyPos = player.scanner.nearestTarget.transform.position;
+        Vector3 dir = targeyPos - transform.position;
+        dir = dir.normalized;
+
+        Transform bullet = GameManager.instance.pool.Get(prefabId).transform;
+        bullet.position = transform.position;
+        bullet.rotation = Quaternion.FromToRotation(Vector3.up, dir);
+        bullet.GetComponent<Bullet>().Init(damage, count , dir );
+
+        AudioManager.instance.playSfx(AudioManager.Sfx.Range);
+    }
+}
